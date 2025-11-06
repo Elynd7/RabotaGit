b59f807baec89325cbeae68cae6c0e54cbf76b5d
Elynd7 <ek2308317@gmail.com>
2025-11-06 13:58:11 +0300

    основное меню и ввод чисел

--- index.php
+++ index.php
@@ -1,105 +1,65 @@
 <?php
 
-$num1 = 0;
-$num2 = 0;
-$numbersEntered = false;
-
-function showMenu() {
-    echo "1. Ввести два числа\n";
-    echo "2. Выполнить сложение\n";
-    echo "3. Выполнить вычитание\n";
-    echo "4. Выполнить деление\n";
-    echo "5. Возвести число в степень\n";
-    echo "6. Выход\n";
-    echo "Выберите пункт: ";
-}
-
-function inputNumbers() {
-    global $num1, $num2, $numbersEntered;
+class Calculator {
+    private $num1;
+    private $num2;
     
-    echo "Первое число: ";
-    $num1 = (float)trim(fgets(STDIN));
-    
-    echo "Второе число: ";
-    $num2 = (float)trim(fgets(STDIN));
-    
-    $numbersEntered = true;
-}
-
-function addNumbers() {
-    global $num1, $num2, $numbersEntered;
-    
-    if (!$numbersEntered) {
-        echo "Сначала введите числа\n";
-        return;
+    public function __construct() {
+        $this->num1 = null;
+        $this->num2 = null;
     }
     
-    echo $num1 + $num2 . "\n";
-}
-
-function subtractNumbers() {
-    global $num1, $num2, $numbersEntered;
-    
-    if (!$numbersEntered) {
-        echo "Сначала введите числа\n";
-        return;
+    public function showMenu() {
+        echo "1. Ввести два числа\n";
+        echo "2. Выполнить сложение\n";
+        echo "3. Выполнить вычитание\n";
+        echo "4. Выполнить деление\n";
+        echo "5. Возвести число в степень\n";
+        echo "6. Выход\n";
+        echo "Выберите пункт меню: ";
     }
     
-    echo $num1 - $num2 . "\n";
-}
+    public function run() {
+        while (true) {
+            $this->showMenu();
+            $choice = trim(fgets(STDIN));
+            
+            switch ($choice) {
+                case '1':
+                    $this->inputNumbers();
+                    break;
+                case '2':
 
-function divideNumbers() {
-    global $num1, $num2, $numbersEntered;
-    
-    if (!$numbersEntered) {
-        echo "Сначала введите числа\n";
-        return;
+                    break;
+                case '3':
+
+                    break;
+                case '4':
+ 
+                    break;
+                case '5':
+
+                    break;
+                case '6':
+                    echo "До свидания!\n";
+                    exit;
+                default:
+                    echo "Неверный выбор. Попробуйте снова.\n";
+            }
+        }
     }
     
-    if ($num2 == 0) {
-        echo "Деление на ноль\n";
-        return;
-    }
-    
-    echo $num1 / $num2 . "\n";
-}
-
-function powerNumbers() {
-    global $num1, $num2, $numbersEntered;
-    
-    if (!$numbersEntered) {
-        echo "Сначала введите числа\n";
-        return;
-    }
-    
-    echo pow($num1, $num2) . "\n";
-}
-
-while (true) {
-    showMenu();
-    $choice = trim(fgets(STDIN));
-    
-    switch ($choice) {
-        case '1':
-            inputNumbers();
-            break;
-        case '2':
-            addNumbers();
-            break;
-        case '3':
-            subtractNumbers();
-            break;
-        case '4':
-            divideNumbers();
-            break;
-        case '5':
-            powerNumbers();
-            break;
-        case '6':
-            exit;
-        default:
-            echo "Неверный выбор\n";
+    private function inputNumbers() {
+        echo "Введите первое число: ";
+        $this->num1 = (float)trim(fgets(STDIN));
+        
+        echo "Введите второе число: ";
+        $this->num2 = (float)trim(fgets(STDIN));
+        
+        echo "Числа сохранены: {$this->num1} и {$this->num2}\n";
     }
 }
 
+$calculator = new Calculator();
+$calculator->run();
 ?>
